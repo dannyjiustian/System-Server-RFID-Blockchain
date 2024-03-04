1c4b536991e10f806b5b3666d2272c15542b4a3f
dannyjiustian <danny.djie72@gmail.com>
2024-03-04 20:15:27 +0700

    call new function for mqtt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 import {
   serverExpress,
   portServerConfig,
+  clientMqtt,
 } from "./api/v1/configs/server.config.js";
 import route from "./api/v1/routes/routes.js";
 
@@ -16,4 +17,14 @@ serverExpress.use(route);
 
 serverExpress.listen(portServerConfig, () => {
   console.log(`Server runing on http://localhost:${portServerConfig}`);
+});
+
+clientMqtt.on("connect", () => {
+  console.log("MQTT Broker Connected on HiveMQ Platfrom!");
+  clientMqtt.subscribe("CheckESP");
+  clientMqtt.subscribe("ReadRFID");
+});
+
+clientMqtt.on("error", (error) => {
+  console.log(`MQTT Broker error, check the error: ${error}`);
 });
